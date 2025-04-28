2e5bd69b424b9677559971fecf60f73013d89647
Michael Faivre <michael.faivre.dspro@gmail.com>
2025-04-28 17:56:30 +0200

    Added Video_Analysis_Webapp/verifications

---
+++ Video_Analysis_Webapp/verifications/build_analysis_json.py
@@ -0,0 +1,283 @@
+
+"""
+Algo:
+1. Read the Json file template for data analysis
+2. Get the data from the video analysis
+3. Create a new Json file with the data from the video analysis from the infos and the template
+"""
+
+import json
+from moviepy.editor import VideoFileClip
+from langdetect import detect
+##import ffmpeg
+##from pprint import pprint
+##from tinytag import TinyTag
+import os
+from datetime import datetime
+import exifread
+
+# Voir si utile de faire une classe ici
+"""
+        "crm_video_type": "consumer review",
+        "nb_speakers": 1,
+        "language": "english",
+        "adult_speaker": 0,
+        "type_environment": "kitchen"
+"sentiment": {
+        "positive_points": [
+            "great camera",
+            "long battery life",
+            "reliable"
+        ],
+        "negative_points": [
+            "small screen"
+        ],
+        "main_sentiment": "positive",
+        "upcoming_purchase_new_item": 1,
+        "budget_for_new_item": 500,
+        "currency": "euro"
+    },
+"content": {
+        "product": "",
+        "brand_tag": 0,
+        "brand": "",
+        "serial_number": "",
+        "list_positive_points": [],
+        "list_negative_points": [],
+        "main_sentiment": "",
+        "upcoming_purchase_new_item": 0,
+        "budget_for_new_item": "",
+        "currency": "euro",
+        "auth_AI_training_usage": 1
+    }
+"""
+
+balises_analyse_video = {"metadata": ["format", "duration_secs", "date", "number_of_words"], 
+                         "context":  ["crm_video_type", "nb_speakers", "language", "adult_speaker", "type_environment"],
+                         "sentiment":["positive_points", "negative_points", "main_sentiment"],
+                         "content":  ["product", "brand_tag", "brand", "serial_number", "list_positive_points", 
+                                      "list_negative_points", "main_sentiment", "upcoming_purchase_new_item",
+                                      "budget_for_new_item", "currency", "auth_AI_training_usage"]}
+
+def read_json_file(json_file):
+    """
+    Read the Json file
+    """
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+def get_format_video(video_path):
+    """
+    Get the format of the video file.
+    """
+    format_video = video_path.split('.')[-1]
+    return format_video
+
+
+def get_duration_video(video_path):
+    clip = VideoFileClip(video_path)
+    return clip.duration
+
+
+def get_date_video(video_path):
+    # Get the date of the video file
+    # ATTTENTION: the processing modifies the creation date of the file to current date
+    creation_time = os.path.getctime(video_path)
+    creation_date = datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d %H:%M:%S')
+    return creation_date
+
+
+def get_geolocation_video(video_path):
+    """
+    Get the geolocation of the video file.
+    code by copilot
+    NB: MP4 files can contain geolocation information, but it is not as commonly embedded as it is in image files like JPEGs. 
+    The presence of geolocation data in an MP4 file depends on how the file was created and the device or software used to record the video.
+    """
+    try:
+        with open(video_path, 'rb') as f:
+            tags = exifread.process_file(f)
+            gps_latitude = tags.get('GPS GPSLatitude')
+            gps_latitude_ref = tags.get('GPS GPSLatitudeRef')
+            gps_longitude = tags.get('GPS GPSLongitude')
+            gps_longitude_ref = tags.get('GPS GPSLongitudeRef')
+
+            print(f'GPS Latitude: {gps_latitude}')
+            print(f'GPS Latitude Ref: {gps_latitude_ref}')
+
+            if gps_latitude and gps_longitude and gps_latitude_ref and gps_longitude_ref:
+                lat = [float(x.num) / float(x.den) for x in gps_latitude.values]
+                lon = [float(x.num) / float(x.den) for x in gps_longitude.values]
+
+                latitude = lat[0] + lat[1] / 60. + lat[2] / 3600.
+                longitude = lon[0] + lon[1] / 60. + lon[2] / 3600.
+
+                if gps_latitude_ref.values[0] != 'N':
+                    latitude = -latitude
+                if gps_longitude_ref.values[0] != 'E':
+                    longitude = -longitude
+
+                return {"latitude": latitude, "longitude": longitude}
+            else:
+                print("No GPS data found in the video file.")
+                return {"latitude": None, "longitude": None}
+    except Exception as e:
+        print(f"Error extracting geolocation: {e}")
+        return {"latitude": None, "longitude": None}
+
+
+def get_number_of_words_speech(text_video):
+    """
+    Get the number of words in the speech of the video.
+    """
+    text_video = text_video.split()
+    return len(text_video)
+
+
+def get_number_of_speakers(text_video):
+    """
+    Get the number of speakers in the speech of the video.
+    """
+    nb_of_persons = 0 # utiliser la fcn de labellisation des objets dans une vidéo
+    return nb_of_persons
+
+
+def get_language(text_video):
+    """
+    Get the language of the speech of the video.
+    """
+    language = detect(text_video)
+    return language
+
+
+def get_currency(text_video):
+    """
+    Get the currency of the speech of the video.
+    1. Find all prices strings in the text
+    2. Modify the price strings as follow: '200 dollars' -> '$200' ; '200 euros' -> '€200'
+    """
+
+    dict_currency = {'dollars': '$', 'euros': '€', 'pounds': '£', 'yen': '¥', 'francs': '₣'}
+    """
+    modified_text = ""
+    word_count = 1
+    list_words = text_video.split()
+    currency = 'unknown'
+    for word in list_words:
+        if word in dict_currency.keys() or word in dict_currency.values():
+            if word in dict_currency.keys():
+                currency = word
+                symbol = dict_currency[word]
+            else:
+                currency = [key for key, val in dict_currency.items() if val == word][0]
+                symbol = word
+            modified_text += symbol + list_words[word_count-1] + " "
+        else:
+            modified_text += word + " "
+        word_count += 1
+            ##currency = word # see to take the most frequent currency
+    text_video = modified_text
+    """
+
+    # Check if the currency is in the text
+    for currency, symbol in dict_currency.items():
+        index_currency = text_video.find(currency)
+        index_symbol = text_video.find(symbol)
+        if index_currency != -1 or index_symbol != -1:
+            return currency
+    # If no currency is found 
+    return 'unknown'
+    
+    """
+    if text_video.find('dollar') !=-1 or text_video.find('$') !=-1:
+        currency = 'dollar'
+    elif text_video.find('euro') !=-1 or text_video.find('€') !=-1:
+        currency = 'euro'
+    elif text_video.find('pound') !=-1 or text_video.find('£') !=-1:
+        currency = 'pound'
+    elif text_video.find('yen') !=-1  or text_video.find('¥') !=-1:
+        currency = 'yen'
+    elif text_video.find('franc') !=-1 or text_video.find('₣') !=-1 in text_video:
+        currency = 'franc'
+    else:
+        currency = 'unknown'
+    """
+    return currency
+
+
+def get_positive_sentiments(text_video):
+    """
+    Get the positive sentiments of the speech of the video.
+    """
+    pass
+
+
+def get_brand_product(text_NER):
+    """
+    Get the brand and product of the speech of the video.
+    """
+    # TEST: NER Text
+    #print('text_NER:', text_NER)
+    #print('text_NER type:', type(text_NER))
+    # convert text_NER to a list of tuples
+    list_NER = eval(text_NER)
+
+    #list_NER = [('IDE', 'ORG'), ('four', 'CARDINAL'), ('8-month', 'DATE'), ('ASUS', 'ORG'), ('VivoBook 17', 'PRODUCT'), ('AI', 'ORG'), ('Google Cloud', 'PRODUCT')]
+    brand = []
+    product = []
+    # coded auto by copilot
+    for entity in list_NER:
+        #print(f'entity: {entity}')
+        #print(f'entity[0]: {entity[0]}')
+        if entity[1] == 'ORG':
+            brand.append(entity[0])
+        elif entity[1] == 'PRODUCT':
+            product.append(entity[0])
+    brand = list(set(brand))
+    product = list(set(product))
+    return brand, product
+
+
+def read_fill_save_json_file(json_file, video_path, text_video, summary_text, text_NER):
+    # read the template analysis Json file
+    video_analysis_template = 'verifications/video_analysis_template.json'
+    json_data = read_json_file(video_analysis_template)
+    if not json_data:
+        print(f'Error: Unable to read the template JSON file: {video_analysis_template}')
+        return None
+
+    # fill metadata format of json_data
+    json_data['metadata']['format'] = get_format_video(video_path)
+    json_data['metadata']['duration_secs'] = get_duration_video(video_path)
+    json_data['metadata']['date'] = get_date_video(video_path)
+    json_data['metadata']['number_of_words'] = get_number_of_words_speech(text_video)
+    json_data['metadata']['geoloc'] = get_geolocation_video(video_path)
+    # fill context format of json_data
+    json_data['context']['crm_video_type'] = ""
+    json_data['context']['nb_speakers'] = get_number_of_speakers(text_video)
+    json_data['context']['language'] = get_language(text_video)
+    json_data['context']['adult_speaker'] = 0
+    # fill content infos of json_data
+    brand, product = get_brand_product(text_NER)
+    json_data['content']['product'] = product
+    json_data['content']['brand_tag'] = 0
+    json_data['content']['brand'] = brand
+    json_data['content']['serial_number'] = ""
+    json_data['content']['NER_tags'] = text_NER
+    json_data['content']['upcoming_purchase_new_item'] = 0
+    json_data['content']['budget_for_new_item'] = ""
+    json_data['content']['currency'] = get_currency(text_video)
+    json_data['content']['auth_AI_training_usage'] = 1
+    # fill sentiment format of json_data
+    json_data['sentiment']['positive_points'] = []
+    json_data['sentiment']['negative_points'] = []
+    json_data['sentiment']['main_sentiment'] = ""
+
+
+    with open(json_file, 'w') as f:
+        json.dump(json_data, f, indent=4)
+    print(f'Json file saved: {json_file}')
+    ###return json_file
+    return True
+    
---
+++ Video_Analysis_Webapp/verifications/check_policy_compliance.py
@@ -0,0 +1,135 @@
+'''
+Purpose: This script checks if a JSON file complies with a specific policy.
+Author: Mike Faivre
+Date: 2025-04-21
+Description: The script reads two JSON files, one containing the policy and 
+the other containing the data to be checked from the Json file obtained from the baseline Analysis
+(text extracted, summary, NER, etc).
+
+Technique: given 2 Json files, how to compare common entries between the 2 files?
+
+ALGO for validation and notation of the uploaded Video w/r the policy Json file:
+1. Read the policy JSON file and the data JSON file.
+2. For each entry in the policy JSON file, check if the corresponding entry exists in the data JSON file.
+3. If the entry exists, compare the value of the data file vs the policy file conditions.
+4. if the value fulfills the conditions, mark it as compliant.
+5. If the entry does not exist or does not fulfill the conditions, mark it as non-compliant.
+6. Store the results in a DataFrame.
+'''
+
+import json
+
+reward_for_authorization = 4 # voir comment le sortir du code !!
+
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except json.JSONDecodeError:
+        print(f"Error: Failed to decode JSON from the file {file_path}.")
+    return None
+
+
+def check_entry_value(section, key, policy_data, analysis_data):
+    # fill with False (treated as 0 in arithmetic), True, reward_for_authorization the dataframe of validation
+    # example: section:'content', key:'brand'
+    if section in policy_data and key in policy_data[section]:
+        # Check if the key exists in the analysis data
+        if section in analysis_data and key in analysis_data[section]:
+            print(f"section: {section}, key: {key}, policy_data[section][key]: {policy_data[section][key]}, analysis_data[section][key]: {analysis_data[section][key]}")
+            if isinstance(policy_data[section][key], list):
+                validation = analysis_data[section][key] in policy_data[section][key]
+                print(f"validation: {validation}")
+                if not validation:
+                    if len(policy_data[section][key]) == 2:
+                        if "date" in key:
+                            # Check if the value is between the two values in the list
+                            validation = policy_data[section][key][0] <= analysis_data[section][key]
+                            return validation
+                        else:
+                            # Check if the value is between the two values in the list
+                            validation = policy_data[section][key][0] <= analysis_data[section][key] <= policy_data[section][key][1]
+                            return validation
+                return validation
+            elif analysis_data[section][key] != "":
+                if "auth" in key:
+                    validation = analysis_data[section][key] * reward_for_authorization # autorisations doivent être "True" ou "False"
+                    return validation
+                else:
+                    validation = True
+                    return validation
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+            
+
+def check_policy_compliance(policy_data, analysis_data):
+    """
+    Check if the JSON data complies with the policy.
+    This function should be implemented based on the specific policy requirements.
+    """
+    # Check compliance for each entry in the data JSON
+    print(f"Checking compliance policy_data: {policy_data}")
+
+    # initialize compliance_dict as a dictionary (NO NEED TO USE A DATAFRAME)
+    compliance_dict = {}
+
+    # Loop of over sections in the policy data
+    for section in policy_data:
+        # Check if the key exists in the analysis data
+        for key in policy_data[section]:
+            print(f"Checking section: {section}, key: {key}")
+            compliance_dict[key] = check_entry_value(section, key, policy_data, analysis_data)
+            print(f"compliance_dict[{key}]:", compliance_dict[key])
+
+    num_policy_checked = len(compliance_dict)
+    print(f"num_policy_checked: {num_policy_checked}")
+    compliance_metrics = sum(compliance_dict.values()) / num_policy_checked if num_policy_checked > 0 else 0
+    print
+    return compliance_dict, compliance_metrics
+
+
+# voir comment connaitre et lire les 2 fichiers: cahier des charges et le fichier d'analyse de base de la vidéo
+### TEST HERE !!
+"""
+# faire une classe ici ?
+policy_data_file = 'cahier_des_charges.json'
+video_analysis_file = 'video_analysis_for_testing.json'
+
+
+# Read the policy JSON file
+policy_file_path = policy_data_file
+policy_data = read_json_file(policy_file_path)
+if policy_data:
+    # Read the data JSON file
+    analysis_data = read_json_file(video_analysis_file)
+
+    # computation of the compliance metrics
+    compliance_dict, compliance_metrics = check_policy_compliance(policy_data, analysis_data)
+    print('compliance_dict computed:', compliance_dict)
+"""
+
+"""
+    for entry in policy_data:
+        if entry == "context":
+            brand = entry.get('brand', 'Unknown')
+            product = entry.get('product', 'Unknown')
+            serial_number = entry.get('serial_number', 'Unknown')
+            # Check if the brand and product are in the analysis data
+            if brand != 'Unknown':
+                :
+"""
+
+# Example usage
+'''
+file_path = 'cahier_des_charges.json'
+json_data = read_json_file(file_path)
+if json_data:
+    print(json_data)
+'''
